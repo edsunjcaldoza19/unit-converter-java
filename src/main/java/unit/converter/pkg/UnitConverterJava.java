a7434814f0eca2e927b8df65d4d064e29ea42ada
EdsunCaldoza <edsunjcaldoza@gmail.com>
2023-07-24 19:45:28 +0800

    First Commit

---
+++ src/main/java/unit/converter/pkg/UnitConverterJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package unit.converter.pkg;
+
+/**
+ *
+ * @author EdSun
+ */
+public class UnitConverterJava {
+
+    public static void main(String[] args) {
+        new UnitConverter().setVisible(true);
+    }
+}
